2e8efff4d88e2d1840dade9793fb98a602c47dfa
Hayashizaki Masayuki / 林崎優之 <akaritakahashioss@gmail.com>
2022-12-13 16:33:25 +0900

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # Python-3-way-handshakes
+This is a simple example of a Python script that can be used to 3-way handshake
+with a server. This is useful for testing the server's ability to handle
+connections.
+
+## Usage
+```
+pip install scapy
+python3 app.py
+Enter the IP address of the server: 
+```
+
+## LICENSE
+MIT License copyright (c) 2022 Takahashi Akari <akaritakahashioss@gmail.com>
+
---
+++ app.py
@@ -0,0 +1,25 @@
+"""
+This is a simple example of a Python script that can be used to 3-way handshake
+with a server. This is useful for testing the server's ability to handle
+connections.
+
+@author: Takahashi Akari
+@date: 2022/12/13
+"""
+
+import random
+from scapy.all import *
+
+sport = random.randint(30000,60000)
+seq = random.randint(1000,2000)
+ip = IP(dst=input('Enter the IP address of the server: '))
+tcp = TCP(sport=sport, dport=80, flags='S', seq=seq)
+
+syn = ip/tcp
+syn_ack = sr1(syn)
+
+tcp.seq = syn_ack.ack
+tcp.ack = syn_ack.seq + 1
+tcp.flags = 'A'
+ack = ip/tcp
+send(ack)
